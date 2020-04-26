48a5a5ec529b1706e3a90edf24de8896e8cf8ae6
dcartist <pbannerman13@gmail.com>
2020-04-25 21:27:43 -0400

    editing routes

--- db/controllers/images.js
+++ db/controllers/images.js
@@ -3,22 +3,9 @@ const router = express.Router();
 const Images = require('../models/Images.js')
  
 router.get("/", (req, res) => {
-    Images.find().then(Images => {
-        res.json(Images)
+    Images.find().then(data => {
+        res.json(data)
     })
-})
-
-router.get("/name/:conLastName", (req, res) => {
-    let theName = req.params.conLastName
-    Images.find({ conLastName: theName }).then(showName => res.json(showName))
-})
-router.get("/id/:id", (req, res) => {
-    let theName = req.params.conLastName
-    Images.find({  _id: req.params.id }).then(showName => res.json(showName))
-})
-
-router.post("/new", (req, res) => {
-    Images.create(req.body).then(Images => res.json(Images))
 })
 
 router.put("/update/:id", (req, res) => {
@@ -26,8 +13,34 @@ router.put("/update/:id", (req, res) => {
         .then(update => res.json(update))
 })
 
-router.delete("/delete/:id", (req, res) => {
-    Images.findOneAndDelete({ _id: req.params.id }).then(deleted => res.json(deleted))
+router.post("/new", (req, res) => {
+    Images.create(req.body).then(Images => res.json(Images))
 })
+
+router.delete("/delete/:id", (req, res) => {
+        Images.findOneAndDelete({ _id: req.params.id }).then(deleted => res.json(deleted))
+    })
+
+// router.get("/name/:conLastName", (req, res) => {
+//     let theName = req.params.conLastName
+//     Images.find({ conLastName: theName }).then(showName => res.json(showName))
+// })
+// router.get("/id/:id", (req, res) => {
+//     let theName = req.params.conLastName
+//     Images.find({  _id: req.params.id }).then(showName => res.json(showName))
+// })
+
+// router.post("/new", (req, res) => {
+//     Images.create(req.body).then(Images => res.json(Images))
+// })
+
+// router.put("/update/:id", (req, res) => {
+//     Images.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+//         .then(update => res.json(update))
+// })
+
+// router.delete("/delete/:id", (req, res) => {
+//     Images.findOneAndDelete({ _id: req.params.id }).then(deleted => res.json(deleted))
+// })
 
 module.exports = router
